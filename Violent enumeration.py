a97ac2d55bde0cf29eda5fb13a3520d1234e4c9b
zkkkara <1994079857@qq.com>
2024-05-18 17:47:23 +0800

    添加了动态规划、贪心枚举和暴力枚举的代码

---
+++ Violent enumeration.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def numSquares(self, n):
+        square_nums = [i**2 for i in range(1, int(math.sqrt(n))+1)]
+
+        def minNumSquares(k):
+            """ recursive solution """
+            # bottom cases: find a square number
+            if k in square_nums:
+                return 1
+            min_num = float('inf')
+
+            # Find the minimal value among all possible solutions
+            for square in square_nums:
+                if k < square:
+                    break
+                new_num = minNumSquares(k-square) + 1
+                min_num = min(min_num, new_num)
+            return min_num
+
+        return minNumSquares(n)
